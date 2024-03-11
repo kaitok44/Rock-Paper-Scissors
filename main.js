4300b98f748ce696c3a89318f6c3e5a53858e1a7
kaitok44 <kaito.2151@gmail.com>
2024-03-11 22:47:39 +0900

    add 5 round playGame function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let playerScore = 0
 let computerScore = 0
+let playerSelection = ""
+let computerSelection = ""
 
 function getComputerChoice(){
   let choice = Math.floor(Math.random()*3);
@@ -18,22 +20,21 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
  if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors"){
     (++playerScore)
-    return "You Win!";
+    console.log("You Win!");
 } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock"){
     (++playerScore)
-    return "You Win!"; 
+    console.log("You Win!"); 
 } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper"){
     (++playerScore)
-    return "You Win!";
+    console.log("You Win!");
 } else if (playerSelection.toLowerCase() === computerSelection){
-    return "Tie!";
+    console.log("Tie!");
 } else {
     (++computerScore)
-    return "Lose!";
+    console.log("Lose!");
 }
 }
-
-function gameWinner (){
+function gameWinner(){
     if (playerScore === computerScore){
         console.log("Its a Tie!");
     } else if (playerScore > computerScore){
@@ -44,5 +45,26 @@ function gameWinner (){
 }
 
 function playGame() {
+ playerSelection = prompt("Rock, Paper, Scissors?");
+ computerSelection = getComputerChoice();
+ playRound(playerSelection, computerSelection);
 
+ playerSelection = prompt("Rock, Paper, Scissors?");
+ computerSelection = getComputerChoice();
+ playRound(playerSelection, computerSelection);
+
+ playerSelection = prompt("Rock, Paper, Scissors?");
+ computerSelection = getComputerChoice();
+ playRound(playerSelection, computerSelection);
+
+ playerSelection = prompt("Rock, Paper, Scissors?");
+ computerSelection = getComputerChoice();
+ playRound(playerSelection, computerSelection);
+
+ playerSelection = prompt("Rock, Paper, Scissors?");
+ computerSelection = getComputerChoice();
+ playRound(playerSelection, computerSelection);
+ gameWinner();
 }
+
+playGame()
