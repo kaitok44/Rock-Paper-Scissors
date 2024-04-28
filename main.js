d94fb931e3bf33433a3f21ea856d8c3fa835c48d
kaitok44 <kaito.2151@gmail.com>
2024-04-28 22:26:42 +0900

    reformat code

--- main.js
+++ main.js
@@ -1,41 +1,40 @@
 let playerScore = 0;
 let compScore = 0;
-let playerSelection = ""
-let compSelection = ""
+let playerSelection = "";
+let compSelection = "";
 
-function getCompChoice(){
-  let choice = Math.floor(Math.random()*3);
-  switch (choice){
+function getCompChoice() {
+  let choice = Math.floor(Math.random() * 3);
+  switch (choice) {
     case 0:
-      return choice = "rock";
+      return (choice = "rock");
       break;
     case 1:
-      return choice = "paper";
-    break;
+      return (choice = "paper");
+      break;
     case 2:
-      return choice =  "scissors";
+      return (choice = "scissors");
   }
 }
 
-function playRound(playerSelection, compSelection){
+function playRound(playerSelection, compSelection) {
   const roundResult = document.querySelector("#roundResult");
- if (playerSelection === "rock" && compSelection === "scissors"){
+  if (playerSelection === "rock" && compSelection === "scissors") {
     playerScore++;
-    roundResult.textContent="You Win!";
-} else if (playerSelection === "paper" && compSelection === "rock"){
-  playerScore++;
-    roundResult.textContent="You Win!"; 
-} else if (playerSelection === "scissors" && compSelection === "paper"){
-  playerScore++;
-   roundResult.textContent="You Win!";
-} else if (playerSelection === compSelection){
-   roundResult.textContent="Tie!";
-} else {
-  compScore++;
-   roundResult.textContent="Lose!";
+    roundResult.textContent = "You Win!";
+  } else if (playerSelection === "paper" && compSelection === "rock") {
+    playerScore++;
+    roundResult.textContent = "You Win!";
+  } else if (playerSelection === "scissors" && compSelection === "paper") {
+    playerScore++;
+    roundResult.textContent = "You Win!";
+  } else if (playerSelection === compSelection) {
+    roundResult.textContent = "Tie!";
+  } else {
+    compScore++;
+    roundResult.textContent = "Lose!";
+  }
 }
-}
-
 
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
@@ -49,43 +48,43 @@ function playGame(playerSelection) {
   compSelection = getCompChoice();
   playRound(playerSelection, compSelection);
   humanScore.textContent = "You " + playerScore;
-  robotScore.textContent = "COM "+ compScore;
+  robotScore.textContent = "COM " + compScore;
 
-  if (playerScore == 5){
+  if (playerScore == 5) {
     gameResult.textContent = "You Win the game!";
     rock.disabled = true;
     paper.disabled = true;
     scissors.disabled = true;
-    playAgain.disabled =  false;
-  } else if (compScore == 5){
-    gameResult.textContent ="You lost. COM wins";
+    playAgain.disabled = false;
+  } else if (compScore == 5) {
+    gameResult.textContent = "You lost. COM wins";
     rock.disabled = true;
     paper.disabled = true;
     scissors.disabled = true;
-    playAgain.disabled =  false;
+    playAgain.disabled = false;
   }
 }
-playAgain.addEventListener("click", ()=>{
+playAgain.addEventListener("click", () => {
   playerScore = 0;
-  compScore  = 0;
+  compScore = 0;
   rock.disabled = false;
   paper.disabled = false;
   scissors.disabled = false;
-  playAgain.disabled =  true;
-  humanScore.textContent = "Human "+ playerScore;
-  robotScore.textContent = "COM "+ compScore;
+  playAgain.disabled = true;
+  humanScore.textContent = "Human " + playerScore;
+  robotScore.textContent = "COM " + compScore;
   roundResult.textContent = "";
-  gameResult.textContent ="";
-})
+  gameResult.textContent = "";
+});
 
-rock.addEventListener("click",() => {
+rock.addEventListener("click", () => {
   playGame("rock");
-})
+});
 
-paper.addEventListener("click",() => {
+paper.addEventListener("click", () => {
   playGame("paper");
-})
+});
 
-scissors.addEventListener("click",() => {
-playGame("scissors");
-})
+scissors.addEventListener("click", () => {
+  playGame("scissors");
+});
